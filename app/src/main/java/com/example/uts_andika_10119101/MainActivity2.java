0a1a7f322dc9fa4a37bac84fc682a16e12747190
Andika Putra <putradika2510@gmail.com>
2022-06-08 01:43:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts_andika_10119101/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.uts_andika_10119101;
+//nim : 10119101
+//nama : Andika Putra
+//kelas : if-3
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
